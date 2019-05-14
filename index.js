ac7a08f457f1c1949cca1a2f7e89ebc7600ec277
novoa97 <novoa199@gmail.com>
2019-05-14 10:49:51 +0200

    Added config final socket

--- index.js
+++ index.js
@@ -14,28 +14,65 @@ const modelsFolder = './models/';
 var fs = require('fs');
 
 
-
-// Obre una pipe de lectura per a llegir del fitxer. No sabem com es crida això
-// app.get('/:model_obj', function(req, res){
-//   var filename = req.params.model_obj;
-//   var readStream = fs.createReadStream('./models/' + filename);
-//   console.log ("INICIA AQUI LA INFO!!!!!!!!!!!");
-//   console.log (readStream);
-//   console.log ("FINALITZA AQUI LA INFO!!!!!!!!");
-//   res.pipe(readStream);
-//   }, function(err) {
-//     throw err;
-// });
-
+// [GET] LLamada para hacer test en el navegador
 app.get('/', function(req,res){
   res.send("Hola")
 })
-// Retorna el fitxer (creiem) o contingut. A postman sembla que retorni contingut..
+
+
+// [GET] Retorna el fitxer (creiem) o contingut. A postman sembla que retorni contingut..
 app.get('/proves/:model_obj', function(req, res){
   var filename = req.params.model_obj;
   res.download(modelsFolder + filename);
   }, function(err) {
     throw err;
+});
+
+
+// [GET] Retorna llista amb tots els fitxers que puc escollir com a model dins la carpeta Models
+app.get('/list', function(req, res){
+  fs.readdir(modelsFolder, (err, files) => {
+    var filesList = [];
+    files.forEach(file => {
+      filesList[filesList.length] = file;
+    });
+    res.send(filesList);
+  }, function(err) {
+    throw err;
+  });
+});
+
+
+// [GET] Retorna el contingut del fitxer nom model_obj
+app.get('/model/:model_obj', function(req, res){
+  var filename = req.params.model_obj;
+  readFile(filename, function(filename, content) { 
+    res.send(content);
+  }, function(err) {
+    throw err;
+  });
+});
+
+
+// [GET] Obre una pipe de lectura per a llegir del fitxer. No sabem com es crida això
+app.get('/:model_obj', function(req, res){
+  var filename = req.params.model_obj;
+  var readStream = fs.createReadStream('./models/' + filename);
+  console.log ("INICIA AQUI LA INFO!!!!!!!!!!!");
+  console.log (readStream);
+  console.log ("FINALITZA AQUI LA INFO!!!!!!!!");
+  res.pipe(readStream);
+  }, function(err) {
+    throw err;
+});
+
+
+// [GET] EndPoint para indicar que modelo queremos reproducir a través de Sockets.io
+app.get('/change/:model_obj', function(req, res){
+  var filename = req.params.model_obj;
+  console.log("[GET] /change/"+filename)
+  io.emit('change',  filename )
+  res.sendStatus(200)
 });
 
 
@@ -54,18 +91,15 @@ app.get('/modulos/:filename', function(req, res){
 */
 
 
-// Retorna llista amb tots els fitxers que puc escollir com a model dins la carpeta Models
-app.get('/list', function(req, res){
-  fs.readdir(modelsFolder, (err, files) => {
-    var filesList = [];
-    files.forEach(file => {
-      filesList[filesList.length] = file;
-    });
-    res.send(filesList);
-  }, function(err) {
-    throw err;
+//Inicializamos Sockets
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('coca', function(msg){
+    console.log('message: ' + msg);
   });
 });
+
 
 //Funcio auxiliar per a la crida de sota
 function readFile(filename, onFileContent, onError) {
@@ -81,29 +115,8 @@ function readFile(filename, onFileContent, onError) {
 }
 
 
-// Retorna el contingut del fitxer nom model_obj
-app.get('/model/:model_obj', function(req, res){
-  var filename = req.params.model_obj;
-  readFile(filename, function(filename, content) { 
-    res.send(content);
-  }, function(err) {
-    throw err;
-  });
-});
 
 
-// EndPoint para indicar que modelo queremos reproducir
-// a través de Sockets.io
-app.get('/change/:model_obj', function(req, res){
-  var filename = req.params.model_obj;
-  io.emit('change',  filename )
-});
-
-
-//Inicializamos Sockets
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
 
 
